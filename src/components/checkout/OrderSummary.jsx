8f609fa812056e504e0c5a138afd0052b86c6eeb
ElaVedena <esther.asanda@gmail.com>
2026-04-18 15:58:20 +0300

    Set shipping prices to 1-5 shillings for all counties for testing purposes

--- src/components/checkout/OrderSummary.jsx
+++ src/components/checkout/OrderSummary.jsx
@@ -1,41 +1,19 @@
- import { useCart } from '../../context/CartContext';
+import { useCart } from '../../context/CartContext';
 import '../../components/css/OrderSummary.css';
 
 function OrderSummary({ selectedCounty }) {
     const { cartItems, totalPrice } = useCart();
 
-    // Shipping costs based on county 
+    // TESTING SHIPPING COSTS - All counties between 1-5 shillings
     const getShippingCost = (county) => {
         if (!county) return 0;
         
-        // Nairobi and surrounding counties
-        const nairobiRegion = ['Nairobi', 'Kiambu', 'Machakos', 'Kajiado'];
-        // Central Kenya counties
-        const centralRegion = ['Muranga', 'Nyeri', 'Kirinyaga', 'Nyandarua', 'Embu', 'Meru', 'Tharaka Nithi'];
-        // Coastal counties
-        const coastalRegion = ['Mombasa', 'Kwale', 'Kilifi', 'Tana River', 'Lamu', 'Taita Taveta'];
-        // Western Kenya counties
-        const westernRegion = ['Kisumu', 'Kisii', 'Nyamira', 'Homa Bay', 'Migori', 'Siaya', 'Vihiga', 'Kakamega', 'Bungoma', 'Busia', 'Trans Nzoia'];
-        // Rift Valley counties
-        const riftRegion = ['Nakuru', 'Uasin Gishu', 'Kericho', 'Bomet', 'Nandi', 'Baringo', 'Laikipia', 'Samburu', 'Turkana', 'West Pokot', 'Elgeyo Marakwet'];
-        // Northern and remote counties (higher shipping cost)
-        const remoteRegion = ['Garissa', 'Wajir', 'Mandera', 'Marsabit', 'Isiolo'];
+        // For testing purposes, return random shipping between 1-5 shillings
+        // Using county name to determine consistent shipping for same county
+        const countyHash = county.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0);
+        const shippingAmount = (countyHash % 5) + 1; // Returns 1, 2, 3, 4, or 5
         
-        if (nairobiRegion.includes(county)) {
-            return 150; // KSh 150 for Nairobi and surrounding
-        } else if (centralRegion.includes(county)) {
-            return 250; // KSh 250 for Central Kenya
-        } else if (coastalRegion.includes(county)) {
-            return 350; // KSh 350 for Coast
-        } else if (westernRegion.includes(county)) {
-            return 300; // KSh 300 for Western Kenya
-        } else if (riftRegion.includes(county)) {
-            return 250; // KSh 250 for Rift Valley
-        } else if (remoteRegion.includes(county)) {
-            return 500; // KSh 500 for remote areas
-        } else {
-            return 400; // Default for other counties
-        }
+        return shippingAmount;
     };
 
     const shipping = selectedCounty ? getShippingCost(selectedCounty) : 0;
@@ -109,4 +87,4 @@ function OrderSummary({ selectedCounty }) {
     );
 }
 
-export default OrderSummary;  
+export default OrderSummary;
